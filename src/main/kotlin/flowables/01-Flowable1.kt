caf99e56a53f0ae40c17ad6e0aba728ae2012fc0
Maikel Chandika <mkdika@gmail.com>
2018-10-09 11:20:24 +0700

    Add Flowable example.

---
+++ src/main/kotlin/flowables/01-Flowable1.kt
@@ -0,0 +1,29 @@
+package flowables
+
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
+
+/**
+ * Flowable are same as Observable, except Flowable have a backpressure feature.
+ * Default buffer for backpressure is 128.
+ */
+fun main(args: Array<String>) {
+    Flowable.range(1, 1000)
+            .map {
+                MyItem(it)
+            }
+            .observeOn(Schedulers.io())
+            .subscribe({
+                println("Received $it")
+                runBlocking { delay(100) }
+            }, { it.printStackTrace() })
+    runBlocking { delay(60000) }
+}
+
+data class MyItem(val id: Int) {
+    init {
+        println("MyItem Created $id")
+    }
+}
